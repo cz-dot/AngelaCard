1664515d502c3a6297f084cb7d675f0f98a81d09
Z <zhouchang29@gmail.com>
2022-10-01 23:18:19 -0700

    Finished Project

--- AngelaCard/ContentView.swift
+++ AngelaCard/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(red:0.09, green:0.64, blue:0.52)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image(systemName: "person.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                    .foregroundColor(.white)
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                
+                Text("a person")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                
+                Text("Title")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                
+                Divider()
+                
+                InfoView(text: "+1 646 123 4567", imageName: "phone.fill")
+                InfoView(text: "person@email.com", imageName: "envelope.fill")
+            }
         }
-        .padding()
     }
 }
 
